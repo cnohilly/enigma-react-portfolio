27a261d505e7f439f1f18aecf0dee12c16bd0b8a
ctnohilly <ctnohilly@gmail.com>
2022-09-19 23:14:48 -0400

    edits to navbar and positioning. added footer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <body className='bg-dark text-light d-flex flex-column min-vh-100'>
+    <div className='bg-dark text-light d-flex flex-column min-vh-100 position-relative'>
       <Nav />
       <main className='flex-grow-1 d-flex flex-column m-2'>
         <About />
       </main>
-      <footer>
-      
-      </footer>
-    </body>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-dark">
+    <header className='sticky-top'>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-secondary">
         <div class="container-fluid">
           <h2><a class="navbar-brand" href="/">Chris Nohilly</a></h2>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -14,16 +14,16 @@ export default function Nav() {
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a href="" className="nav-link">About Me</a>
+                <a href="#about" className="nav-link">About Me</a>
               </li>
               <li className="nav-item">
-                <a href="" className="nav-link">Portfolio</a>
+                <a href="#portfolio" className="nav-link">Portfolio</a>
               </li>
               <li className="nav-item">
-                <a href="" className="nav-link">Contact</a>
+                <a href="#contact" className="nav-link">Contact</a>
               </li>
               <li className="nav-item">
-                <a href="" className="nav-link">Resume</a>
+                <a href="#resume" className="nav-link">Resume</a>
               </li>
             </ul>
           </div>
