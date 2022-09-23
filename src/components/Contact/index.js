74939952b235cd5df54df71ac8adb7fbb1faca1f
ctnohilly <ctnohilly@gmail.com>
2022-09-22 23:58:07 -0400

    created email validation and helper functions and completed form validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { capitalizeFirstLetter, validateEmail } from '../../utils/helpers';
+
 
 function Contact() {
+
+    // const inputs = [
+    //     {
+    //         name: 'email',
+    //         element: document.querySelector("input[name='email']")
+    //     },
+    //     {
+    //         name: 'name',
+    //         element: document.querySelector("input[name='name']")
+    //     },
+    //     {
+    //         name: 'message',
+    //         element: document.querySelector("textarea[name='message']")
+    //     }
+    // ]
+
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
+    const [formComplete, setFormComplete] = useState(false);
+    // use effect to determine if a form is complete with values
+    useEffect(() => {
+        setFormComplete(Object.values(formState).every(input => {
+            return input;
+        }))
+    }, [formState]);
+
+    // function to call when a form field loses focus
+    // variable used to hold error message to avoid async issues
+    function handleChange(e) {
+        let errorText = '';
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                errorText = 'Please enter a valid email.';
+            }
+        } else {
+            if (!e.target.value.length) {
+                errorText = `${capitalizeFirstLetter(e.target.name)} is required.`;
+            }
+        }
+        if (errorText) {
+            setErrorMessage(errorText, setFormState({ ...formState, [e.target.name]: '' }));
+        } else {
+            setErrorMessage('', setFormState({ ...formState, [e.target.name]: e.target.value }));
+        }
+    }
+
+    // handles submitting the form, verifies that there is currently no error message and the form is completed
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!errorMessage && formComplete) {
+            console.log(formState);
+        }
+    }
+
     return (
-        <section className="content-section container mt-3" id="contact">
-            <form action="submit" id="contact-form">
+        <section className="content-section container my-3" id="contact">
+            <form action="submit" id="contact-form" onSubmit={handleSubmit}>
                 <h3 className='mb-3'>Contact Me!</h3>
                 <div className='form-floating mb-3'>
-                    <input type="text" className="form-control" id="floatInput" placeholder='Name' />
+                    <input name='name' type="text"
+                        className="form-control" id="floatInput"
+                        placeholder='Name' onBlur={handleChange} />
                     <label htmlFor="floatInput">Name</label>
                 </div>
                 <div className="form-floating mb-3">
-                    <input type="email" placeholder="Email" className="form-control" id="emailInput" />
+                    <input name='email' type="email"
+                        className="form-control" id="emailInput"
+                        placeholder="Email" onBlur={handleChange} />
                     <label htmlFor="emailInput">Email</label>
                 </div>
                 <div className="form-floating mb-3">
-                    <textarea id="messageInput" className="form-control" placeholder='Type a message'></textarea>
+                    <textarea name='message' id="messageInput"
+                        className="form-control" placeholder='Type a message'
+                        onBlur={handleChange}></textarea>
                     <label htmlFor="messageInput">Message</label>
                 </div>
                 <div className="float-sm-end d-flex align-items-center justify-content-sm-center flex-column flex-sm-row">
-                    <span className='text-danger mx-3 order-last order-sm-first'>Please enter a valid email.</span>
+                    <span className='text-danger mx-3 order-last order-sm-first'>{errorMessage}</span>
                     <button className="btn btn-success mb-3 mb-sm-0 px-5 my-auto order-first order-sm-last" id="submit-btn">Submit</button>
                 </div>
             </form>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,7 @@ export function validateEmail(email) {
     const re = /[0-9A-z-_.]+[0-9A-z]+@[0-9A-z]+[0-9A-z.]+[0-9A-z]+\.[A-z]+[A-z.]+[A-z]+/;
     return re.test(String(email));
 }
+
+export function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
