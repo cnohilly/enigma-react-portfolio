2c9a4568f904d28ef3f544c5876b51af71084e4d
ctnohilly <ctnohilly@gmail.com>
2022-09-27 16:30:31 -0400

    small changes and navbar fixes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 export default function Nav({ currentPage, handlePageChange }) {
+
+  function toggleNavBar() {
+    const toggle = document.getElementById('page-navbar-toggler');
+    if (toggle.getAttribute('aria-expanded') === 'true') {
+      toggle.click();
+    }
+  }
+
   return (
     <header className='sticky-top'>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-secondary sticky-top">
         <div className="container-fluid">
-          <h2 ><a className="navbar-brand text-light fs-3" id='name-title' href="./">Chris Nohilly</a></h2>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+          <h2 ><a className="navbar-brand text-light fs-3" id='name-title'
+            href="#about" onClick={() => handlePageChange('About')}>Chris Nohilly</a></h2>
+          <button id='page-navbar-toggler' className="navbar-toggler" type="button" data-bs-toggle="collapse"
             data-bs-target="#page-navbar" aria-controls="page-navbar" aria-expanded="false"
             aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -14,32 +23,32 @@ export default function Nav({ currentPage, handlePageChange }) {
           <div className="collapse navbar-collapse" id="page-navbar">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a href="#about" data-bs-toggle="collapse" data-bs-target="#page-navbar"
-                  onClick={() => handlePageChange('About')}
+                <a href="#about"
+                  onClick={() => { handlePageChange('About'); toggleNavBar(); }}
                   className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
                 >
                   About Me
                 </a>
               </li>
               <li className="nav-item">
-                <a href="#portfolio" data-bs-toggle="collapse" data-bs-target="#page-navbar"
-                  onClick={() => handlePageChange('Portfolio')}
+                <a href="#portfolio"
+                  onClick={() => { handlePageChange('Portfolio'); toggleNavBar(); }}
                   className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
                 >
                   Portfolio
                 </a>
               </li>
               <li className="nav-item">
-                <a href="#contact" data-bs-toggle="collapse" data-bs-target="#page-navbar"
-                  onClick={() => handlePageChange('Contact')}
+                <a href="#contact"
+                  onClick={() => { handlePageChange('Contact'); toggleNavBar(); }}
                   className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
                 >
                   Contact
                 </a>
               </li>
               <li className="nav-item">
-                <a href="#resume" data-bs-toggle="collapse" data-bs-target="#page-navbar"
-                  onClick={() => handlePageChange('Resume')}
+                <a href="#resume"
+                  onClick={() => { handlePageChange('Resume'); toggleNavBar(); }}
                   className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
                 >
                   Resume
